4c64f250cfcaa3f6dddb9a84a603ce803397e4d5
luccaamvs <49735799+luccaamvs@users.noreply.github.com>
2021-09-21 17:50:28 -0300

    via upload

---
+++ desafio/src/test/java/com/gbp/java/webapplication/crud/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbp.java.webapplication.crud.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
